7a05dd6bbe860ba3d115e7e11e081072d3efd030
Teodor Todorov <banskaliqtabg@gmail.com>
2017-02-27 17:07:08 +0200

    Listing 1.2.

---
+++ springinaction/src/main/java/com/springinaction/knights/DamselRescuingKnight.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.springinaction.knights;
+
+/**
+ *
+ * @author Teodor Todorov
+ */
+public class DamselRescuingKnight implements Knight {
+
+    private RescueDamselQuest quest;
+
+    public DamselRescuingKnight() {
+        this.quest = new RescueDamselQuest();
+    }
+
+    public void embarkOnQuest() {
+        quest.embark();
+    }
+}
---
+++ springinaction/src/main/java/com/springinaction/knights/RescueDamselQuest.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.springinaction.knights;
+
+/**
+ *
+ * @author Teodor Todorov
+ */
+class RescueDamselQuest {
+
+    void embark() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
