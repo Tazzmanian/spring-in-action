29975f92c4cf598cd655108744eac29bdd325266
Teodor Todorov <banskaliqtabg@gmail.com>
2017-03-02 15:04:34 +0200

    Listing 2.5

---
+++ springinaction/src/test/java/com/springinaction/soundsystem/CDPlayerTest.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.springinaction.soundsystem;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ *
+ * @author Teodor Todorov
+ */
+@RunWith(SpringRunner.class)
+//@SpringBootTest
+@ContextConfiguration(classes = CDPlayerConfig.class)
+public class CDPlayerTest {
+
+    @Autowired
+    private CompactDisk compactDisk;
+
+    @Test
+    public void cdShouldNotBeNull() {
+        Assert.assertNotNull(compactDisk);
+    }
+}
