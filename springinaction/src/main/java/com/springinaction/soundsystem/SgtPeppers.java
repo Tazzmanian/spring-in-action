da8a6f2524a60b52b287787eaf827462dac76e63
Teodor Todorov <banskaliqtabg@gmail.com>
2017-03-02 14:49:50 +0200

    Listing 2.2

---
+++ springinaction/src/main/java/com/springinaction/soundsystem/SgtPeppers.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.springinaction.soundsystem;
+
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author Teodor Todorov
+ */
+@Component
+public class SgtPeppers implements CompactDisk {
+
+    private String title = "Sgt. Pepper's Lonely Hearts Club Band";
+    private String artist = "The Beatles";
+
+    @Override
+    public void play() {
+        System.out.println("Playing " + title + " by " + artist);
+    }
+
+}
