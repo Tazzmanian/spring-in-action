f8a245f1dbc9f67d6c0fa2f65a064ddd3e569239
Teodor Todorov <banskaliqtabg@gmail.com>
2017-03-06 10:55:09 +0200

    Scooped beans

---
+++ springinaction/src/main/java/com/springinaction/scoopedbeans/UniqueThing.java
@@ -0,0 +1,13 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.springinaction.scoopedbeans;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UniqueThing {
+    // the details of this class are inconsequential to this example
+}
---
+++ springinaction/src/test/java/com/springinaction/scoopedbeans/ComponentScannedConfig.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.springinaction.scoopedbeans;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(excludeFilters = {
+    @Filter(type = FilterType.ANNOTATION, value = Configuration.class)})
+public class ComponentScannedConfig {
+
+}
